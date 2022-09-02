823d381f4ceafee45adbf912e46af22722f0ca1b
Md. Hasan Taraque <108396624+mdhasantaraque@users.noreply.github.com>
2022-09-03 03:42:38 +0600

    Add files via upload

--- Tabloid world/js/tabloid.js
+++ Tabloid world/js/tabloid.js
@@ -1,7 +1,21 @@
-const loadCategories= ()=>{
+// Set the categories button
+const loadCategories= () =>{
         fetch('https://openapi.programming-hero.com/api/news/categories')
         .then(res => res.json())
-        .then(data => console.log(data.data.news_category))
+        .then(data => setMenu(data.data.news_category))
         
+}
+
+const setMenu = (categories) =>{
+        const categoriesField = document.getElementById('categories-field')
+        for(const category of categories){
+                // console.log(category.category_name);
+                const li = document.createElement('li');
+                li.classList.add('li-field')
+                li.innerHTML=`
+                       <a> ${category.category_name}</a>
+                `
+                categoriesField.appendChild(li);
+        }
 }
 loadCategories();
