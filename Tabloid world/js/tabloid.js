03bea89d14b3e8e0deef30344847e9c72a20c5fa
Md. Hasan Taraque <108396624+mdhasantaraque@users.noreply.github.com>
2022-09-03 09:51:52 +0600

    Add files via upload

--- Tabloid world/js/tabloid.js
+++ Tabloid world/js/tabloid.js
@@ -3,41 +3,47 @@ const loadCategories= () =>{
         fetch('https://openapi.programming-hero.com/api/news/categories')
         .then(res => res.json())
         .then(data => setMenu(data.data.news_category))
-        
+        // .catch(error => console.log(error))
 }
 
 const setMenu = (categories) =>{
         const categoriesField = document.getElementById('categories-field')
         for(const category of categories){
-                // console.log(category.category_name);
+                // console.log(category.category_id);
                 const li = document.createElement('li');
                 li.classList.add('li-field');
                 li.innerHTML=`
-                       <a> ${category.category_name}</a>
+                       <button onclick="newsLoads('${category.category_id}')"> ${category.category_name}</button>
                 `
                 categoriesField.appendChild(li);
         }
 }
 loadCategories();
 
-const newsLoads = () =>{
-        fetch('https://openapi.programming-hero.com/api/news/category/01')
+/*-----------------News Portal--------------------*/
+
+const newsLoads = (code) =>{
+        const url =`https://openapi.programming-hero.com/api/news/category/${code}`
+        console.log('get news details', url);
+        fetch(url)
         .then(res => res.json())
         .then(data => newsSection(data.data))
+        .catch(error => console.log(error))
 }
 
 
 const newsSection = allNews =>{
         const newsField = document.getElementById('news-field');
+        newsField.innerHTML ='';
         allNews.forEach(news =>{
-                console.log(news);
+                // console.log(news);
         const newsDiv = document.createElement('div');
         newsDiv.innerHTML= `
         <div class="card lg:card-side bg-base-100 shadow-xl my-6">
                 <figure><img src="${news.thumbnail_url}" alt="Album"></figure>
                 <div class="card-body">
                         <h2 class="card-title">${news.title}</h2>
-                        <div class="h-28 p-4 text-ellipsis overflow-hidden">
+                        <div class="h-28 p-4  text-ellipsis overflow-hidden">
                            <p>${news.details}</p>
                         </div>
                         
@@ -49,13 +55,13 @@ const newsSection = allNews =>{
                                         </div>
                                         </label>
                                         <div>
-                                        <strong>${news.author.name === 'system'? 'No data ava ilable':news.author.name }</strong><br>
+                                        <strong>${news.author.name == null? 'No data available':news.author.name }</strong><br>
                                         <span>${news.author.published_date}</span>
                                         </div>
                                 </div>
                                 <div>
                                         <h1 class="font-medium inline">View </h1>
-                                        <p class="inline font-medium"> ${news.total_view === 0?' No data ava ilable':news.total_view }</P>
+                                        <p class="inline font-medium"> ${news.total_view === 0 ||news.total_view == null ?' No data available':news.total_view }</P>
 
                                 </div>
                                 <div>
@@ -70,4 +76,4 @@ const newsSection = allNews =>{
 
         })
 }
-newsLoads();
+// newsLoads();
