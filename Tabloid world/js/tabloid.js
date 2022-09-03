ad049558dd7d9479cdaf85dcc9af446b8f1175c3
Taraque <mtaraque32@gmail.com>
2022-09-03 16:37:58 +0600

    blog section added

--- Tabloid world/js/tabloid.js
+++ Tabloid world/js/tabloid.js
@@ -9,7 +9,6 @@ const loadCategories= () =>{
 const setMenu = (categories) =>{
         const categoriesField = document.getElementById('categories-field')
         for(const category of categories){
-                // console.log(category.category_id);
                 const li = document.createElement('li');
                 li.classList.add('li-field');
                 li.innerHTML=`
@@ -24,7 +23,6 @@ loadCategories();
 
 const newsLoads = (code) =>{
         const url =`https://openapi.programming-hero.com/api/news/category/${code}`
-        console.log('get news details', url);
         fetch(url)
         .then(res => res.json())
         .then(data => newsSection(data.data))
@@ -36,10 +34,8 @@ const newsSection = allNews =>{
         const newsField = document.getElementById('news-field');
         const totalArticles = document.getElementById('total-articles')
         totalArticles.innerText = allNews.length;
-
         newsField.innerHTML ='';
-        allNews.forEach(news =>{
-                // console.log(news);
+        allNews.map(news =>{
         const newsDiv = document.createElement('div');
         newsDiv.innerHTML= `
         <div class="card lg:card-side bg-base-100 shadow-xl my-6">
@@ -81,32 +77,47 @@ const newsSection = allNews =>{
         })
 }
 /*-------------Modal field---------------*/
-const displayModalField = id =>{
-        const url =`https://openapi.programming-hero.com/api/news/category/${id}`        
+const displayModalField = idNo =>{
+        const url =`https://openapi.programming-hero.com/api/news/category/${idNo}` 
+        // console.log(url);       
         fetch(url)
         .then(res => res.json())
         .then(data => allModals(data.data))
+        .catch(error => console.log(error))
 }
 const allModals = modals =>{
-        console.log(modals);
+        // console.log(modals);
         const modalSection = document.getElementById('modal-section');
-        modals.forEach(modal=>{
-                console.log(modal);
+        for(const modal of modals){
+                // console.log(modal);
                 const modalDiv = document.createElement('div')
                 modalDiv.innerHTML =`
-                <input type="checkbox" id="my-modal-6" class="modal-toggle" />
+                <input type="checkbox" id="my-modal-6" class="modal-toggle"/>
                 <div class="modal modal-bottom sm:modal-middle">
                         <div class="modal-box">
-                                <h3 class="font-bold text-lg">Congratulations random Internet user!</h3>
-                                <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+                                <h3 class="font-bold text-lg">C${modal.title}</h3>
+                                <p class="py-4">${modal.details}</p>
+                                <div class="card-actions flex justify-between px-6">
+                                <div class="flex items-center">
+                                        <label tabindex="0" class="btn-circle avatar">
+                                        <div class="w-10 rounded-full">
+                                                 <img src="${modal.author.img}" />
+                                        </div>
+                                        </label>
+                                        <div>
+                                        <strong>${modal.author.name == null || modal.author.name == ''? 'No data available': modal.author.name }</strong><br>
+                                        <span>${modal.author.published_date}</span>
+                                        </div>
+                                </div>
+                                </div>
                                 <div class="modal-action">
-                                        <label for="my-modal-6" class="btn">close</label>
+                                        <label for="my-modal-6" class="btn btn-primary">close</label>
                                 </div>
                         </div>
                 </div>
                 `
                 modalSection.appendChild(modalDiv);
-        })
+        }
                 
       
 }
