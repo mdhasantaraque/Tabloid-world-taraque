9e541054d73575b67c6bcb77189c7ebd55731343
Taraque <mtaraque32@gmail.com>
2022-09-03 11:58:52 +0600

    added article field

--- Tabloid world/js/tabloid.js
+++ Tabloid world/js/tabloid.js
@@ -3,7 +3,7 @@ const loadCategories= () =>{
         fetch('https://openapi.programming-hero.com/api/news/categories')
         .then(res => res.json())
         .then(data => setMenu(data.data.news_category))
-        // .catch(error => console.log(error))
+        .catch(error => console.log(error))
 }
 
 const setMenu = (categories) =>{
