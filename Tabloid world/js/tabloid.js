06171191ec58aca0f47416448bed366e8fe78e07
Taraque <mtaraque32@gmail.com>
2022-09-03 13:42:20 +0600

    modal field added

--- Tabloid world/js/tabloid.js
+++ Tabloid world/js/tabloid.js
@@ -68,7 +68,8 @@ const newsSection = allNews =>{
 
                                 </div>
                                 <div>
-                                        <button class="btn btn-primary">Details</button>
+                                    <label onclick="displayModalField('${news.category_id}')" for="my-modal-6" class="btn btn-primary modal-button">open modal</label>
+                                        
                                 </div>
                         </div>
 
@@ -79,4 +80,33 @@ const newsSection = allNews =>{
 
         })
 }
-newsLoads(' ');
+/*-------------Modal field---------------*/
+const displayModalField = id =>{
+        const url =`https://openapi.programming-hero.com/api/news/category/${id}`        
+        fetch(url)
+        .then(res => res.json())
+        .then(data => allModals(data.data))
+}
+const allModals = modals =>{
+        console.log(modals);
+        const modalSection = document.getElementById('modal-section');
+        modals.forEach(modal=>{
+                console.log(modal);
+                const modalDiv = document.createElement('div')
+                modalDiv.innerHTML =`
+                <input type="checkbox" id="my-modal-6" class="modal-toggle" />
+                <div class="modal modal-bottom sm:modal-middle">
+                        <div class="modal-box">
+                                <h3 class="font-bold text-lg">Congratulations random Internet user!</h3>
+                                <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+                                <div class="modal-action">
+                                        <label for="my-modal-6" class="btn">close</label>
+                                </div>
+                        </div>
+                </div>
+                `
+                modalSection.appendChild(modalDiv);
+        })
+                
+      
+}
